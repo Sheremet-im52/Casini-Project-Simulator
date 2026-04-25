a6a4186df697ccfb930381fb9427dce5367a0c8c
Влад Шеремет <_@example.com>
2026-04-25 16:59:11 +0300

    поле генерація

--- public/app.js
+++ public/app.js
@@ -1,9 +1,21 @@
 const grid = document.getElementById("grid");
+const spinButton = document.getElementById("spin");
+const symbols = ["🐶", "🍖", "⭐", "💀", "1", "2", "3", "4", "5", "6", "7"];
 
-// поле 
-for (let i = 0; i < 36; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.innerText = "?";
-    grid.appendChild(cell);
+function generateGrid() {
+    grid.innerHTML = "";
+
+    for (let i = 0; i < 36; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        // випадковий символ
+        const randomIndex = Math.floor(Math.random() * symbols.length);
+        cell.innerText = symbols[randomIndex];
+
+        grid.appendChild(cell);
+    }
 }
+spinButton.addEventListener("click", () => {
+    generateGrid();
+});
